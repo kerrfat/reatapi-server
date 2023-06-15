f8a9a81887313fbcd006f09460c4c2b70a843684
Fateh K <kfateh2007@gmail.com>
2023-06-15 14:54:40 -0700

    ignore data

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-       res.send('Hello, fateh');
+       res.send('Hello, Tahar');
 });
 
 app.listen(port, () => {
